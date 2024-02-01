eee558e3b982c704d67b4c6e3914c2373e01f423
Nikolayad <145016164+Nikolayad@users.noreply.github.com>
2024-02-01 03:29:01 +0300

    Add files via upload

---
+++ HomeworkMobilOperator.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    float vremya;
+    short oper;
+    setlocale(LC_ALL, "");
+    cout << "Сколько минут длился ваш разговор?\n";
+    cin >> vremya;
+    if (vremya >= 0) {
+        cout << "Ваш мобильный оператор:(1,2,3)\n";
+        cin >> oper;
+        switch (oper) {
+        case 1:
+            cout << "Ваш разговор стоит " << vremya * 4.1 << " рублей";
+            break;
+        case 2:
+            cout << "Ваш разговор стоит " << vremya * 2.3 << " рублей";
+            break;
+        case 3:
+            cout << "Ваш разговор стоит " << vremya * 1.9 << " рублей";
+            break;
+        default:
+            cout << "Ошибка ";
+        }
+    }
+    else
+        cout << "Введенные данные некорректны";
+}
+
