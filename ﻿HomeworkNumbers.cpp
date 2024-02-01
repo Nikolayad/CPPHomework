39511117f8e043913b5c3c83c6f8505d32077ebf
Nikolayad <145016164+Nikolayad@users.noreply.github.com>
2024-02-01 03:19:05 +0300

    Add files via upload

---
+++ ﻿HomeworkNumbers.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int a, b, c = 0;
+    bool h = 1;
+    cout << "Введите диапозон\n";
+    cin >> a >> b;
+    if (a > b)swap(a, b);
+    for (int i = a; i <= b; i++) {
+        h = 1;
+        for (int j = 2; i > j; j++) {
+            if (i % j == 0) {
+                h = 0;
+                break;
+            }
+        }
+        if (h == 1)c++;
+    }
+    cout << "В диапозоне " << c << " простых значений";
+}
